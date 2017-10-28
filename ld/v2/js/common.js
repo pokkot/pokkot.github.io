a386bb1f42b15238cd52f81b02abb16f2ce7cfdb
Oleg Evgrashin <o.evgrashin@gmail.com>
2017-10-29 03:07:45 +0700

    Crated contact and feedback sections. Tried other colors.

---
+++ ld/v2/js/common.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+	$("#portfolio_grid").mixItUp();
+	$(".s_portfolio li").click(function(){
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	windowSizeDetect();
+	$(window).resize(function() {
+		windowSizeDetect();
+	})
+	function windowSizeDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+
+
+	//triangle menu
+	$(".sandwich-icon, .triangle-menu-list li a").click(function() {
+		$(".triangle-menu-list, .triangle-menu").toggleClass("open");
+	});
+
+	$(".triangle-menu-list li a").mPageScroll2id();
+
+
+	//simple menu
+	$(".toggle_menu, .top_menu a").click(function() {
+		$(".sandwich").toggleClass("active");
+		$(".top_menu").fadeToggle(600);
+		$(".top_menu li a").toggleClass("fadeInUp animated");
+		$(".top_text").toggleClass("low_opacity");
+	});
+
+	$(".top_menu ul a").mPageScroll2id();
+
+	/*$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});*/
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+	$(".main_head .social_wrap li").animated("zoomIn", "zoonOut");
+	$(".cont_photo").animated("fadeInLeft", "fadeOutDown");
+	$(".cont_list").animated("fadeInRight", "fadeOutDown");
+
+}); 
